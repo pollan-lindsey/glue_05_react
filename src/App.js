818c79341bbd1b4bb6e995cc2ec9257c6b29059a
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-16 22:05:29 -0500

    i think this is all?

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
+import React, {Component} from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import Footer from './Footer'
 
-export default function App() {
-  return ( 
-    <div>
-      <h1>Glue 5 lab</h1>
-      <nav 
-          style={{
-            borderBottom: 'solid 1px',
-            paddingBottom: '1rem'
-          }}
-      >
-        <Link to='/main'>Main</Link> |{''}
-        <Link to='/contact'>Contact</Link>
-      </nav>
-      <Outlet />
-      <Footer />
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return ( 
+      <div>
+        <h1>Glue 5 lab</h1>
+        <nav 
+            style={{
+              borderBottom: 'solid 1px',
+              paddingBottom: '1rem'
+            }}
+        >
+          <Link to='/main'>Main</Link> |{''}
+          <Link to='/contact'>Contact</Link>
+        </nav>
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  }
 }
---
+++ src/routes/main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <main style={{ padding: '1rem 0'}}>
+            <h2>Main</h2>
+        </main>
+    )
+}
